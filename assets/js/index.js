1640c83501a5d89648bde4e9232db2175c09d995
Anurag <anurag@infinitysoftsystems.com.au>
2023-04-03 08:37:18 +0530

    integrated the delete feature as well

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,7 +7,7 @@ $("#update_user").submit(function (event) {
     var data = {};
     var unindexed_array = $(this).serializeArray();
     $.map(unindexed_array, function (n, i) {
-        data[n["name"]] = n.value
+        data[n["name"]] = n.value;
     })
     console.log(data);
     var request = {
@@ -20,3 +20,22 @@ $("#update_user").submit(function (event) {
         alert("Data updated successfully")
     })
 });
+
+if (window.location.pathname == '/') {
+    $ondelete = $(".table tbody td a.delete");
+    $ondelete.click(function () {
+        var id = $(this).attr("data-id")
+        var request = {
+            "url": `http://localhost:3000/api/users/${id}`,
+            "method": "DELETE"
+        }
+
+
+        if (confirm("Do you really want to delete this user")) {
+            $.ajax(request).done(function (response) {
+                alert("Data deleted sucessfully")
+                location.reload();
+            })
+        }
+    })
+}
